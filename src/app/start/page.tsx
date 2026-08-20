49199381dacd0c749cdca2fddc5852bb46dac5e1
Subrata Roy <roy.subrata2099@gmail.com>
2026-08-20 10:34:25 -0400

    Redirect start to start-here

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,8 +1,5 @@
+﻿import { redirect } from "next/navigation";
+
 export default function StartPage() {
-    return (
-        <main className="p-10 text-black">
-            <h1>Start Here</h1>
-            <p>Welcome to NomadLifeXP system.</p>
-        </main>
-    );
+    redirect("/start-here");
 }

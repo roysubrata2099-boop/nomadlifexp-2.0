742aec1717de6d332ce19a1c2a8ee69ebb17b0e7
Subrata Roy <roy.subrata2099@gmail.com>
2026-08-26 19:34:23 -0400

    fix insights route redirect build

--- src/app/insights/[slug]/page.tsx
+++ src/app/insights/[slug]/page.tsx
@@ -1,4 +1,4 @@
-﻿import { redirect, notFound } from "next/navigation";
+﻿import { redirect } from "next/navigation";
 
 type PageProps = {
     params: Promise<{
@@ -35,3 +35,4 @@ export default async function InsightsRedirect({
 
     redirect(`/blog/posts/${targetSlug}`);
 }
+
